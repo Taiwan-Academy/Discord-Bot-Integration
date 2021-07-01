33d2a8f67d071a2c8bfecd7fe860b57473d9f0f4
LuisHsu <luishsuplus@gmail.com>
2021-07-01 05:11:43 +0000

    Fix submodule

--- IntroductionBot/__init__.py
+++
@@ -1,9 +0,0 @@
-from Bot import Bot
-
-class IntroductionBot(Bot):
-    
-    def on_ready(self):
-        print("IntroductionBot ready")
-
-    def on_message(self, message):
-        print("IntroductionBot message: [{}] {}".format(message.author, message.content)) # FIXME:
