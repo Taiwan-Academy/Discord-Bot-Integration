1b35d8697661e9d6baed27ae032125ca44f88084
brian86258 <brian86258@gmail.com>
2021-07-02 20:31:52 +0800

    DB_singleton

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from API import API
+from DB import DB
 from keep_alive import keep_alive
 
 from IntroductionBot import IntroductionBot
@@ -6,6 +7,8 @@ from IntroductionBot import IntroductionBot
 if __name__ == '__main__':
     bots = [IntroductionBot()]
     api = API(bots)
+    db1 = DB()
+    # db Usage: db.create(), db.add() ..... 
 
     # run
     keep_alive()
