82d82401a5d7079ac8ff76b241f5c17d9abbdedd
brian86258 <brian86258@gmail.com>
2021-07-13 23:34:37 +0800

    Complete DB ORM transform

---
+++ db_example_usage.py
@@ -0,0 +1,68 @@
+from DB import DB
+
+
+if __name__ =='__main__':
+    db = DB()
+    # Following implement some handy function at first
+    # if need some advanced query or operation can direct access db.session to operate
+
+    # Get all allumni in the same university, KEY: if university not created yet, will be error
+    alums = db.get_university_alums('CMU')
+    for alumni in alums:
+        print(alumni.user_id)
+    
+    # Add new university, May can read from a json file.
+    univ_list=[]
+    univ_list.append({
+        'univ_abbrev':"NYU",
+        'univ_name':"New York University",
+        'region':'New York'
+    })
+    univ_list.append({
+        'univ_abbrev':"CMU",
+        'univ_name':"Carneige Mellon University",
+        'region':'Pittsburgh'
+    })
+    univ_list.append({
+        'univ_abbrev':"UCI",
+        'univ_name':"UC Irvine",
+        'region':'Irvine'
+    })
+    # add_universities will read a list of univ dict, univ_abbrev is required
+    db.add_universities(univ_list) 
+
+
+
+    # Add new user, May can read from a json file.
+    user = {
+        "user_id" : "brian#862236"
+    }
+    db.add_users(user)
+
+    # Update user info, by find user by user_id,
+    user_id = 'brian#862236'
+    user_info = {
+        'user_name' : "Brian Chen",
+        'univ_abbrev' : "CMU",
+    }
+    db.update_user_by_ID(user_id, user_info)
+
+    # Same operation for update_univ_by_abbrev
+    univ_abbrev = 'CMU'
+    univ_info = {
+        'univ_name' : "Carneigee Mellon University",
+    }
+    db.update_univ_by_abbrev(univ_abbrev, univ_info)
+
+
+    # get user object by ID
+    user = db.get_user_by_ID('brian#862236')
+    # Note should directly access its attributes
+    print(user.user_id)
+    print(user.user_name)
+    print(user.univ_abbrev)
+    print(user.user_status)
+
+
+
+
